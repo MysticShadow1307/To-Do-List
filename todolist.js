ccca359da6ed1290246454be2cfddb34bbcaf7b2
MysticShadow1307 <71941643+MysticShadow1307@users.noreply.github.com>
2022-11-02 23:56:22 -0400

    Delete todolist.js

--- todolist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-var Todolist = mongoose.model('Todolist', {
-    text: {type: String },
-    isCompleted: {type: String},
-    priority: {type: Number}
-});
-
-module.exports = {Todolist};
